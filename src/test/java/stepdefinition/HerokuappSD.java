97b61d253b4ab35610a01c0e68212c38e9a68acb
[Tahir Qayyum] <[qayyumtahir00@gmail.com]>
2018-11-27 22:19:40 -0500

    Cleaning up code

--- src/test/java/stepdefinition/HerokuappSD.java
+++ src/test/java/stepdefinition/HerokuappSD.java
@@ -10,36 +10,8 @@ public class HerokuappSD {
 
     private HerokuappPage ds = new HerokuappPage();
 
-    @Given("^I am on darksky website homepage$")
-    public void darkskyHomepage() { }
+    @Given("^I am on Herokuapp website homepage$")
+    public void HerokuappHomepage() { }
 
-    @When("^I clear search text field$")
-    public void clearSearch() { ds.clearText(); }
 
-    @And("^I enter an (.+) into the search field$")
-    public void setText(String expectedText){ ds.sendText(expectedText); }
-
-    @And("^I click on search magnifying glass$")
-    public void clickOnSearch() { ds.clickSearch(); }
-
-    @Then("^Verify current temperature is between low and high$")
-    public void verifyTemp() throws InterruptedException { }
-
-    //@darksky-2
-    @Then("^I verify timeline is displayed with two hours incremented$")
-    public void verifyTimeline() throws InterruptedException { ds.verifyTimeline(); }
-
-    //@darksky-3
-    @When("^I expand today's timeline$")
-    public void clickTimeline() { ds.clickOnTimeline(); }
-
-    @Then("^I verify lowest and highest temp is displayed correctly$")
-    public void verifyLowAndHigh() { ds.verifyLowAndHighTemp(); }
-
-    //@darksky-4
-    @When("^I click on Time Machine$")
-    public void clickOnTime() {ds.clickOnTimeMachine();}
-
-    @Then("^I verify date is selected to today's date$")
-    public void verifyCurrentDate() { ds.verifyCurrentDate(); }
 }
